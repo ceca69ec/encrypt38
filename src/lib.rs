83c7bb9485471be7ea1ba531626aa5fc703fe27a
ceca69ec <86930570+ceca69ec@users.noreply.github.com>
2021-07-22 15:11:22 +0000

    Fixed typo on documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -88,7 +88,7 @@
 //! ## Installation
 //!
 //! You have to install [rust](https://www.rust-lang.org/tools/install) and a
-//!  [liker](https://gcc.gnu.org/wiki/InstallingGCC) if you don't already have them.
+//!  [linker](https://gcc.gnu.org/wiki/InstallingGCC) if you don't already have them.
 //!
 //! ```shell
 //! $ cargo install encrypt38
